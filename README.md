90a74d8ff8c853f267390a331f49560afc9a64b4
Jameel137 <gamegg35@yahoo.com>
2020-12-15 10:49:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-gb76me
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-gb76me)
---
+++ index.ts
@@ -0,0 +1,5 @@
+let num1, num2, num3, number;
+num1 = parseInt(prompt("insertnum1"));
+num2 = parseInt(prompt("insertnum2"));
+num3 = num1 + num2;
+console.log(num3);
